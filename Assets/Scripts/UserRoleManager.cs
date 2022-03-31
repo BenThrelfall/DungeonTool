8ef900f2d29d6ae526126f313dfdf988fef2c014
Benjamin Threlfall <threlfall.b@gmail.com>
2022-03-31 12:07:44 +0100

    Added basic user role support

---
+++ Assets/Scripts/UserRoleManager.cs
@@ -0,0 +1,45 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserRoleManager : NetworkBehaviour {
+
+    bool usersHaveJoined = false;
+
+    [SerializeField]
+    List<GameObject> dmObjects = new List<GameObject>();
+
+    public override void OnStartClient() {
+        base.OnStartClient();
+
+        CmdUserJoined();
+    }
+
+    [Command(requiresAuthority = false)]
+    void CmdUserJoined(NetworkConnectionToClient conn = null) {
+        if (usersHaveJoined) {
+            TargetSetAsPlayer(conn);
+        }
+        else {
+            TargetSetAsDM(conn);
+            usersHaveJoined = true;
+        }
+    }
+
+    [TargetRpc]
+    void TargetSetAsPlayer(NetworkConnection conn) {
+
+    }
+
+    [TargetRpc]
+    void TargetSetAsDM(NetworkConnection conn) {
+
+        foreach (var obj in dmObjects) {
+            obj.SetActive(true);
+        }
+
+    }
+
+
+}
